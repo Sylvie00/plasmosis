5f81554e7fc767d70ecf003b25e7a5536500878a
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-18 00:19:48 -0500

    attempt one at building vtp

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,23 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+dnf install -y steam tailscale 
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-dnf5 install -y tmux 
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+cat > /usr/share/ublue-os/image-info.json <<EOF
+{
+  "image-name": "Plasmosis,
+  "image-vendor": "Sylvie00",
+  "image-ref": "plasmosis",
+  "image-tag": "latest",
+  "image-branch": "main",
+  "base-image-name": "Fedora Kinoite",
+  "fedora-version": "$(rpm -E %fedora)",
+  "version": "$(rpm -E %fedora)",
+  "version-pretty": "$(rpm -E %fedora)"
+}
+EOF
 
 systemctl enable podman.socket
+systemctl disable plasmalogin.service
+systemctl enable sddm.service
