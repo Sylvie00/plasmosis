d92f91cdbcb70922c171ad8f0a6c351cd925f73e
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-24 13:26:53 -0500

    add -y

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,7 +6,7 @@ dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-
 dnf install -y steam tailscale
 
 curl -sSL "https://github.com/dahui/z13ctl/releases/download/v1.1.6/z13ctl_1.1.6_linux_amd64.rpm" -o /tmp/z13ctl.rpm
-dnf install /tmp/z13ctl.rpm
+dnf install -y /tmp/z13ctl.rpm
 z13ctl setup && rm /tmp/z13ctl.rpm
 
 systemctl enable podman.socket
