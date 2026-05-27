547f1682ed06ec91679e706a68a431496e64eef1
Sylvie00 <taylorkellogg540@gmail.com>
2026-05-26 20:06:47 -0500

    revert last change

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,5 +8,3 @@ set -ouex pipefail
 cd /lib/firmware/cirrus
 sudo ln -sf cs35l41/bincfgs/cs35l41-dsp1-19_5dB.bincfg.xz cs35l41-dsp1-spk-prot-10431fb3-l0.bincfg.xz
 sudo ln -sf cs35l41/bincfgs/cs35l41-dsp1-19_5dB.bincfg.xz cs35l41-dsp1-spk-prot-10431fb3-r0.bincfg.xz
-
-systemctl enable reload-hid_asus.service
